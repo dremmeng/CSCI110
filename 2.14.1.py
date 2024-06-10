459ace1891f2a576beed1d002f15de66207f3151
dremmeng <44462821+dremmeng@users.noreply.github.com>
2024-06-10 08:09:57 -0600

    Add files via upload

---
+++ 2.14.1.py
@@ -0,0 +1,11 @@
+all="All"
+work="work"
+an="and"
+no="no"
+play="play"
+makes="makes"
+jack="Jack"
+a="a"
+dull="dull"
+boy="boy"
+print(all+" "+work+" "+an+" "+no+" "+play+" "+makes+" "+jack+" "+a+" "+dull+" "+boy+".")
