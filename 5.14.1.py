7f1ce1a44b77a2e4dde86e7b2af1457ff6320ebe
dremmeng <44462821+dremmeng@users.noreply.github.com>
2024-06-10 13:23:20 -0600

    Add files via upload

---
+++ 5.14.1.py
@@ -0,0 +1,21 @@
+
+def daynumber(day):
+    if day == 0:
+        return "Sunday"
+    elif day == 1:
+        return "Monday"
+    elif day == 2:
+        return "Tuesday"
+    elif day == 3:
+        return "Wednesday"
+    elif day == 4:
+        return "Thursday"
+    elif day == 5:
+        return "Friday"
+    elif day == 6:
+        return "Saturday"
+
+
+day = int(input("What is the day number?"))
+str=daynumber(day)
+print(str)
