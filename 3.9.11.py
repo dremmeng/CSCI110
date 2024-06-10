3adc51268b117fe116f179a075a94f81471e84de
dremmeng <44462821+dremmeng@users.noreply.github.com>
2024-06-10 09:18:39 -0600

    Add files via upload

---
+++ 3.9.11.py
@@ -0,0 +1,15 @@
+# import for turtle
+import turtle
+
+# Starting a Working Screen
+ws = turtle.Screen()
+
+# initializing a turtle instance
+turtle = turtle.Turtle()
+
+#Loop 5 times
+for i in [1,2,3,4,5]:
+    #moving turtle 100 units forward
+    turtle.forward(100)
+    #rotating turtle 144 degree right
+    turtle.right(144)
