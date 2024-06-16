86df891fc030bb1f4ce61e818d05726bbf0ab4f8
dremmeng <44462821+dremmeng@users.noreply.github.com>
2024-06-16 15:42:38 -0600

    Add files via upload

--- problem.py
+++ problem.py
@@ -1,4 +1,9 @@
-def getMinimumPrice(int pirce1,int price2, int bundlePrice, int number1, int number2):
-    minimumPrice = 0
-    return minimumPrice
+def getMinimumPrice(price1,price2, bundlePrice, number1, number2):
     
+    if number1==0 and number2 == 0:
+        return 0
+    if number1 == 0:
+        return number2*price2
+    if number2 == 0:
+        return number1*price1
+    return min(price1+getMinimumPrice(price1, price2, bundlePrice, number1-1, number2), price2+getMinimumPrice(price1, price2, bundlePrice, number1, number2-1), bundlePrice+getMinimumPrice(price1,price2, bundlePrice, number1-1,number2-1))
